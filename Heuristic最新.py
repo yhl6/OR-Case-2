93d4edb6017e275d9eebec9308bcdee6e0e64218
yhl6 <75846041+yhl6@users.noreply.github.com>
2021-05-08 16:24:55 +0800

    add heu

---
+++ Heuristic最新.py
@@ -0,0 +1,167 @@
+from gurobipy import *
+import pandas as pd
+import time
+import math
+
+def heuristic_algorithm(file_path,n):
+
+	start = time.time()
+	local_time = time.ctime(start)
+	df = pd.read_excel(file_path)
+	
+	#需求
+	Demand_info = pd.read_excel(file_path, 'Demand')
+	Demandlist = []
+	for i in Demand_info.index:
+		Demandlist.append(list(Demand_info.loc[i]))
+
+	for i in range(len(Demandlist)):
+		Demandlist[i].pop(0)
+	#print(Demandlist)
+
+	# 產品、月份、運送方法編號 i k t
+	ProductID = range(len(Demand_info['Product']))
+	Shipping_method = range(0, 3)
+	MonthID = range(0, 26)
+
+	# 初始存貨
+	Initial_Inv = pd.read_excel(file_path, 'Initial inventory')
+	Initial_Inventory = Initial_Inv['Initial inventory']
+	#print(Initial_Inventory)
+
+	# 運送費用
+	Shipping_Cost = pd.read_excel(file_path, 'Shipping cost')
+	Express_delivery = Shipping_Cost['Express delivery']
+	Air_freight = Shipping_Cost['Air freight']
+	Fixed_cost = [100, 80, 50]
+
+	# 在途存貨
+	In_transit = pd.read_excel(file_path, 'In-transit')
+	April_intransit = In_transit[2]
+	May_intransit = In_transit[3]
+
+	# 箱子大小(第二題)
+	Size = pd.read_excel(file_path, 'Size')
+	Cubic_meter = Size['Size']
+
+	# 售價、購買成本、期末持有成本
+	Price_info = pd.read_excel(file_path, 'Price and cost')
+	Sales = Price_info['Sales price']
+	Purchasing_cost = Price_info['Purchasing cost']
+	Holding_cost = Price_info['Holding cost']
+
+	# Shortage & Backorder
+	Shortage = pd.read_excel(file_path,'Shortage')
+	Backorder = Shortage['Backorder'] # 0.05 p_i
+	Lost_sales = Shortage['Lost sales'] # 機會成本
+	Backorder_percentage = Shortage['Backorder percentage'] # 願意繼續等的機率
+
+	# Bounds & Vendor
+	Bounds = pd.read_excel(file_path,'Bounds')
+	Minimum_order = Bounds['Minimum order quantity (if an order is placed)']
+	Vendor_product = pd.read_excel(file_path,'Vendor-Product')
+	VendorC = pd.read_excel(file_path,'Vendor cost')
+	Vendor_cost = VendorC['Ordering cost']
+
+	# Conflict 產品1,2
+	Conflict = pd.read_excel(file_path, 'Conflict')
+	Conflict_product = Conflict[['Product 1','Product 2']]
+
+
+	#每期若完全不訂貨，會缺多少產品 
+	x = []
+	for i in ProductID:
+		x.append([])
+
+	#期末存貨，假設前提是每期的貨缺少的貨，都是當月才到，也就是說一旦缺貨，之後的期末存貨就都是0
+	y = []
+	for i in ProductID:
+		y.append([])
+
+
+	#算x,y
+	Left = 0
+	for i in ProductID:
+		Left = 0
+
+		if Backorder_percentage[i] >= 2:
+			NotList.append(i)
+		for t in MonthID:   #26 period
+				
+				if t == 0:
+					Left = Initial_Inventory[i] - Demandlist[i][t]
+				elif t == 1:
+					Left = Left - Demandlist[i][t] + April_intransit[i]
+				elif t == 2:
+					Left = Left - Demandlist[i][t] + May_intransit[i]
+				else:
+					Left = Left - Demandlist[i][t]
+
+				if Left >= 0:
+					x[i].append(0)
+					y[i].append(Left)
+				elif Left < 0:
+					x[i].append(Left*(-1))
+					y[i].append(0)
+					Left = 0
+	#如果低於lowerbound就補
+	for i in ProductID:
+		for t in MonthID:
+			if Minimum_order[i] > x[i][t] and x[i][t] != 0:
+				x[i][t] = Minimum_order[i]
+				y[i][t] += Minimum_order[i] - x[i][t]
+
+	Ocean_cost = []
+	for i in ProductID:
+		Ocean_C = (1500/0.5) * Cubic_meter[i]
+		Ocean_cost.append(Ocean_C)
+	
+	#船運費用比空運小就紀錄
+	Ship = []
+	Air = []
+	for i in ProductID:
+		if Ocean_cost[i] < Air_freight[i]:
+			Ship.append(i)
+			#print(i+1,Ocean_cost[i],Air_freight[i])
+
+	for i in ProductID:
+		if i not in Ship:
+			Air.append(i)
+
+
+	#objective values   假設所有第三周以後都用空運，除非船運比空運便宜
+	z = (sum(Express_delivery[i]*x[i][1] for i in ProductID) 
+	 + sum(Air_freight[i]*x[i][t] for t in range(2,26) for i in Air)
+	 + 1500 * (sum(math.ceil((x[i][t]*Cubic_meter[i])/0.5) for i in Ship for t in range(2,26)))
+	 + sum(Purchasing_cost[i]*x[i][t] for t in MonthID for i in ProductID)
+	 + sum(Holding_cost[i]*y[i][t] for t in MonthID for i in ProductID)
+	 + sum(Lost_sales[i]*(1-Backorder_percentage[i])*x[i][0] for i in ProductID)
+	 + Fixed_cost[0] + Fixed_cost[1]*25
+	 + (Vendor_cost[0]+Vendor_cost[1])*26 )
+
+
+
+	OptimalList = [1282438647,164832708,542768823,868342211,1102028614]
+	print('z =',format(z,','))
+	print('optimal gap =',round((z-OptimalList[n])/OptimalList[n] * 100,4),'%')
+	
+	end = time.time()
+	local = time.ctime(end)
+	print('start: ',local_time)
+	print('end: ',local)
+	print('time =',round(end-start,4),'s')
+	print('-----------------------------')
+
+file1 = 'OR109-2_case02_data_s1.xlsx'
+file2 = 'OR109-2_case02_data_s2.xlsx'
+file3 = 'OR109-2_case02_data_s3.xlsx'
+file4 = 'OR109-2_case02_data_s4.xlsx'
+file5 = 'OR109-2_case02_data_s5.xlsx'
+
+heuristic_algorithm(file1,0)
+heuristic_algorithm(file2,1)
+heuristic_algorithm(file3,2)
+heuristic_algorithm(file4,3)
+heuristic_algorithm(file5,4)
+
+
--- newp2.py
+++ newp2.py
@@ -10,179 +10,183 @@ locale.setlocale( locale.LC_ALL, '' )
 '''
 讀取資料
 '''
-for num in range(1, 6):
-        start = time.time()
-        DATA_PATH = Path(__file__).resolve().parent / 'data'
-        file_name = DATA_PATH / f'OR109-2_case02_data_s{num}.xlsx'
-        df = pd.ExcelFile(file_name)
-        Demand = pd.read_excel(df, sheet_name='Demand', index_col=0)
+for num in [1]:
+    start = time.time()
+    DATA_PATH = Path(__file__).resolve().parent / 'data'
+    file_name = DATA_PATH / f'OR109-2_case02_data_s{num}.xlsx'
+    df = pd.ExcelFile(file_name)
+    Demand = pd.read_excel(df, sheet_name='Demand', index_col=0)
 
-        # 期初存貨
-        Initial_inventory = pd.read_excel(df, sheet_name='Initial inventory', index_col=0)
-        Initial_inv = Initial_inventory['Initial inventory']
+    # 期初存貨
+    Initial_inventory = pd.read_excel(df, sheet_name='Initial inventory', index_col=0)
+    Initial_inv = Initial_inventory['Initial inventory']
 
-        # 變動運輸成本
-        Shipping_cost = pd.read_excel(df, sheet_name='Shipping cost', index_col=0)
-        Express_delivery = Shipping_cost['Express delivery']
-        Air_freight = Shipping_cost['Air freight']
+    # 變動運輸成本
+    Shipping_cost = pd.read_excel(df, sheet_name='Shipping cost', index_col=0)
+    Express_delivery = Shipping_cost['Express delivery']
+    Air_freight = Shipping_cost['Air freight']
 
-        # 在途存貨
-        In_transit = pd.read_excel(df, sheet_name='In-transit', index_col=0)
+    # 在途存貨
+    In_transit = pd.read_excel(df, sheet_name='In-transit', index_col=0)
 
-        # 產品體積
-        Size = pd.read_excel(df, sheet_name='Size', index_col=0)
-        Cubic_meter = Size['Size']
+    # 產品體積
+    Size = pd.read_excel(df, sheet_name='Size', index_col=0)
+    Cubic_meter = Size['Size']
 
-        # 售價、購買成本、持有成本
-        Price_and_cost = pd.read_excel(df, sheet_name='Price and cost', index_col=0)
-        Sales = Price_and_cost['Sales price']
-        Purchasing_cost = Price_and_cost['Purchasing cost']
-        Holding_cost = Price_and_cost['Holding cost']
+    # 售價、購買成本、持有成本
+    Price_and_cost = pd.read_excel(df, sheet_name='Price and cost', index_col=0)
+    Sales = Price_and_cost['Sales price']
+    Purchasing_cost = Price_and_cost['Purchasing cost']
+    Holding_cost = Price_and_cost['Holding cost']
 
-        # 銷售順延成本、銷售損失成本、銷售順延比率
-        Shortage = pd.read_excel(df, sheet_name='Shortage', index_col=0)
-        Backorder = Shortage['Backorder']  # 0.05 p_i
-        Lost_sales = Shortage['Lost sales']  # 機會成本
-        Backorder_percentage = Shortage['Backorder percentage']  # 願意繼續等的機率
+    # 銷售順延成本、銷售損失成本、銷售順延比率
+    Shortage = pd.read_excel(df, sheet_name='Shortage', index_col=0)
+    Backorder = Shortage['Backorder']  # 0.05 p_i
+    Lost_sales = Shortage['Lost sales']  # 機會成本
+    Backorder_percentage = Shortage['Backorder percentage']  # 願意繼續等的機率
 
-        # 其他資料
-        Vendor_product = pd.read_excel(df, sheet_name='Vendor-Product', index_col=0)
-        ProductID_grouped = Vendor_product.groupby('Vendor')
+    # 其他資料
+    Vendor_product = pd.read_excel(df, sheet_name='Vendor-Product', index_col=0)
+    ProductID_grouped = Vendor_product.groupby('Vendor')
 
-        Vendor_cost = pd.read_excel(df, sheet_name='Vendor cost', index_col=0)
-        Ordering_cost = Vendor_cost['Ordering cost']
+    Vendor_cost = pd.read_excel(df, sheet_name='Vendor cost', index_col=0)
+    Ordering_cost = Vendor_cost['Ordering cost']
 
-        Bounds = pd.read_excel(df, sheet_name='Bounds', index_col=0)
-        Lower_bound = Bounds['Minimum order quantity (if an order is placed)']
-        Conflict = pd.read_excel(df, sheet_name='Conflict', index_col=0)
-        Conflict_tl = tuplelist(zip(Conflict['Product 1'], Conflict['Product 2']))
+    Bounds = pd.read_excel(df, sheet_name='Bounds', index_col=0)
+    Lower_bound = Bounds['Minimum order quantity (if an order is placed)']
+    Conflict = pd.read_excel(df, sheet_name='Conflict', index_col=0)
+    Conflict_tl = tuplelist(zip(Conflict['Product 1'], Conflict['Product 2']))
 
-        Fixed_cost = [100, 80, 50]
+    Fixed_cost = [100, 80, 50]
 
-        N, M = Demand.shape  # 產品數、月份數
-        R = Vendor_cost.shape[0]  # 供應商的數量
-        J = Conflict.shape[0]  # 衝突的數量(共有幾種衝突)
+    N, M = Demand.shape  # 產品數、月份數
+    R = Vendor_cost.shape[0]  # 供應商的數量
+    J = Conflict.shape[0]  # 衝突的數量(共有幾種衝突)
 
-        ProductID = range(1, N + 1)
-        MonthID = range(1, M + 1)
-        Shipping_method = range(1, 4)
-        VendorID = range(1, R + 1)
-        ConflictID = range(1, J + 1)
+    ProductID = range(1, N + 1)
+    MonthID = range(1, M + 1)
+    Shipping_method = range(1, 4)
+    VendorID = range(1, R + 1)
+    ConflictID = range(1, J + 1)
 
-        # TD[i] = Total demand for product i
-        Total_demand = sum(Demand.apply(lambda x: x.sum(), axis=1))
+    # TD[i] = Total demand for product i
+    Total_demand = sum(Demand.apply(lambda x: x.sum(), axis=1))
 
-        # 模型的部分
-        p2 = Model("p2")
-        p2.Params.LogToConsole = 0
-        # Non-binary Variables
-        x = p2.addVars(ProductID, Shipping_method, range(0, M + 1), lb=0, vtype=GRB.INTEGER, name='x')  # x[i, k, t]
-        v = p2.addVars(ProductID, range(0, M + 1), lb=0, vtype= GRB.CONTINUOUS, name= 'v')  # 期末存貨 v[i, t]
-        s = p2.addVars(ProductID, range(0, M + 1), lb=0, vtype= GRB.CONTINUOUS, name= 's')  # Shortage s[i, t]
-        g = p2.addVars(MonthID, lb=0, vtype=GRB.INTEGER, name='g')  # 高斯符號 g[t]
-        D = p2.addVars(ProductID, MonthID, vtype=GRB.CONTINUOUS, name='D')  # Demand of product i in week t as considering backorder
+    # 模型的部分
+    p2 = Model("p2")
+    p2.Params.LogToConsole = 0
+    # Non-binary Variables
+    x = p2.addVars(ProductID, Shipping_method, range(0, M + 1), lb=0, vtype=GRB.INTEGER, name='x')  # x[i, k, t]
+    v = p2.addVars(ProductID, range(0, M + 1), lb=0, vtype= GRB.CONTINUOUS, name= 'v')  # 期末存貨 v[i, t]
+    s = p2.addVars(ProductID, range(0, M + 1), lb=0, vtype= GRB.CONTINUOUS, name= 's')  # Shortage s[i, t]
+    g = p2.addVars(MonthID, lb=0, vtype=GRB.INTEGER, name='g')  # 高斯符號 g[t]
+    D = p2.addVars(ProductID, MonthID, vtype=GRB.CONTINUOUS, name='D')  # Demand of product i in week t as considering backorder
 
-        # Binary Variables
-        z = p2.addVars(Shipping_method, MonthID, lb=0, vtype=GRB.BINARY, name='z')  # z[k, t] = 1 if method k is used in month t
-        d = p2.addVars(ProductID, MonthID, lb=0, vtype=GRB.BINARY, name='d')  # d[i, t] = 1 if v[i, t] = 0 or = 0 otherwise.
-        a = p2.addVars(ProductID, MonthID, lb=0, vtype=GRB.BINARY, name='a')  # a[i, t] = 1 if product i is ordered in month t or = 0 otherwise.
-        o = p2.addVars(VendorID, MonthID, lb=0, vtype=GRB.BINARY, name='o')  # o[r, t] = 1 if we order any product from vendor r
+    # Binary Variables
+    z = p2.addVars(Shipping_method, MonthID, lb=0, vtype=GRB.BINARY, name='z')  # z[k, t] = 1 if method k is used in month t
+    d = p2.addVars(ProductID, MonthID, lb=0, vtype=GRB.BINARY, name='d')  # d[i, t] = 1 if v[i, t] = 0 or = 0 otherwise.
+    a = p2.addVars(ProductID, MonthID, lb=0, vtype=GRB.BINARY, name='a')  # a[i, t] = 1 if product i is ordered in month t or = 0 otherwise.
+    o = p2.addVars(VendorID, MonthID, lb=0, vtype=GRB.BINARY, name='o')  # o[r, t] = 1 if we order any product from vendor r
 
-        # 更新變數
-        p2.update()
+    # 更新變數
+    p2.update()
 
-        # 目標式
+    # 目標式
 
-        p2.setObjective(
-            quicksum(Express_delivery[i] * quicksum(x.select(i, 1, '*')) for i in ProductID)  # 陸運變動成本
-            + quicksum(Air_freight[i] * quicksum(x.select(i, 2, '*')) for i in ProductID)  # 空運變動成本
-            + quicksum(Fixed_cost[k - 1] * quicksum(z.select(k, '*')) for k in Shipping_method)  # 固定成本
-            + quicksum(Purchasing_cost[i] * quicksum(x.select(i, '*', '*')) for i in ProductID)  # 購買成本
-            + quicksum(Holding_cost[i] * quicksum(v[i, t] for t in MonthID) for i in ProductID)  # 倉儲成本
-            + 1500 * quicksum(g.select())  # 貨櫃成本
-            + quicksum(quicksum(Lost_sales[i] * s[i, t] * (1 - Backorder_percentage[i]) for i in ProductID) for t in MonthID)  # Lost sales cost
-            + quicksum(quicksum(Backorder[i] * Backorder_percentage[i] * s[i, t] for i in ProductID) for t in MonthID)  # Backorder cost
-            + quicksum(Ordering_cost[r] * quicksum(o[r, t] for t in MonthID) for r in VendorID), GRB.MINIMIZE)  # Vendor cost
+    p2.setObjective(
+        quicksum(Express_delivery[i] * quicksum(x.select(i, 1, '*')) for i in ProductID)  # 陸運變動成本
+        + quicksum(Air_freight[i] * quicksum(x.select(i, 2, '*')) for i in ProductID)  # 空運變動成本
+        + quicksum(Fixed_cost[k - 1] * quicksum(z.select(k, '*')) for k in Shipping_method)  # 固定成本
+        + quicksum(Purchasing_cost[i] * quicksum(x.select(i, '*', '*')) for i in ProductID)  # 購買成本
+        + quicksum(Holding_cost[i] * quicksum(v[i, t] for t in MonthID) for i in ProductID)  # 倉儲成本
+        + 1500 * quicksum(g.select())  # 貨櫃成本
+        + quicksum(quicksum(Lost_sales[i] * s[i, t] * (1 - Backorder_percentage[i]) for i in ProductID) for t in MonthID)  # Lost sales cost
+        + quicksum(quicksum(Backorder[i] * Backorder_percentage[i] * s[i, t] for i in ProductID) for t in MonthID)  # Backorder cost
+        + quicksum(Ordering_cost[r] * quicksum(o[r, t] for t in MonthID) for r in VendorID), GRB.MINIMIZE)  # Vendor cost
 
 
-        # 限制式
+    # 限制式
 
-        p2.addConstrs((v[i, 0] == 0) for i in ProductID)
-        p2.addConstrs((s[i, 0] == 0) for i in ProductID)
+    p2.addConstrs((v[i, 0] == 0) for i in ProductID)
+    p2.addConstrs((s[i, 0] == 0) for i in ProductID)
 
-        for i in ProductID:
-            for k in Shipping_method:
-                p2.addConstr(x[i, k, 0] == 0)
+    for i in ProductID:
+        for k in Shipping_method:
+            p2.addConstr(x[i, k, 0] == 0)
 
-        # y[i, t] Constraint
-        for i in ProductID:
-            p2.addConstr((v[i, 1] == v[i, 0] + s[i, 1] + In_transit.loc[i, 1] - D[i, 1] + Initial_inv[i]), 'Week 1')
-            p2.addConstr((v[i, 2] == v[i, 1] + s[i, 2] + In_transit.loc[i, 2] - D[i, 2] + x[i, 1, 1] ), 'Week 2')
-            p2.addConstr((v[i, 3] == v[i, 2] + s[i, 3] + In_transit.loc[i, 3] - D[i, 3] + x[i, 1, 2] + x[i, 2, 1] ), 'Week 3')
+    # y[i, t] Constraint
+    for i in ProductID:
+        p2.addConstr((v[i, 1] == v[i, 0] + s[i, 1] + In_transit.loc[i, 1] - D[i, 1] + Initial_inv[i]), 'Week 1')
+        p2.addConstr((v[i, 2] == v[i, 1] + s[i, 2] + In_transit.loc[i, 2] - D[i, 2] + x[i, 1, 1] ), 'Week 2')
+        p2.addConstr((v[i, 3] == v[i, 2] + s[i, 3] + In_transit.loc[i, 3] - D[i, 3] + x[i, 1, 2] + x[i, 2, 1] ), 'Week 3')
 
-        for i in ProductID:
-            for t in MonthID[3:]:
-                p2.addConstr((v[i, t] == v[i, t - 1] + s[i, t] + x[i, 1, t - 1] + x[i, 2, t - 2] + x[i, 3, t - 3] - D[i, t]), 'Week others')
+    for i in ProductID:
+        for t in MonthID[3:]:
+            p2.addConstr((v[i, t] == v[i, t - 1] + s[i, t] + x[i, 1, t - 1] + x[i, 2, t - 2] + x[i, 3, t - 3] - D[i, t]), 'Week others')
 
-        # Z Constraint
+    # Z Constraint
+    for t in MonthID:
+        for k in Shipping_method:
+            p2.addConstr(quicksum(x[i, k, t] for i in ProductID) <= Total_demand * z[k, t], 'Z constraint')
+
+    # Y的binary constraint
+    for i in ProductID:
+        for t in MonthID:
+            p2.addConstr(Total_demand * d[i, t] >= s[i, t])
+            p2.addConstr(Total_demand * (1 - d[i, t]) >= v[i, t])
+            p2.addConstr(D[i, t] >= Demand.loc[i, t] + Backorder_percentage[i] * s[i, t - 1])
+
+    # ceiling constraints
+    for t in MonthID:
+        p2.addConstr(g[t] >= quicksum(Cubic_meter[i] * x[i, 3, t] for i in ProductID) / 0.5)
+
+    # conflict constraint
+    for i, j in Conflict_tl:
+        for t in MonthID:
+            p2.addConstr((a[i, t] + a[j, t] <= 1))
+
+    # o[r, t] constraints
+    for r in VendorID:
+        for t in MonthID:
+            for i in [ProductID_grouped.groups[r]]:
+                p2.addConstr((quicksum(x.select(i, '*', t))) <= Total_demand * o[r, t])
+
+    # Lower Bound constraint
+    p2.addConstrs((quicksum(x.select(i, '*', t)) >= Lower_bound[i] * a[i, t]) for i in ProductID for t in MonthID)
+    p2.addConstrs((quicksum(x.select(i, '*', t)) <= Total_demand * a[i, t]) for i in ProductID for t in MonthID)
+
+    p2.optimize()
+
+    # print(f's{num}\'s objVal: {locale.currency(p2.objVal, grouping=True)}')
+    print(p2.objVal)
+
+
+    arr = []
+    arr1 = []
+    for i in range(1, M + 1):
+        for j in range(3):
+            arr1.append(str(i))
+    arr = [arr1, ['E', 'A', 'O'] * M]
+    tuples = list(zip(*arr))
+    row_index = pd.Index([*range(1, N + 1)], name='Product')
+    # print(tuples)
+    index = pd.MultiIndex.from_tuples(tuples, names=['Month', 'Method'])
+    new_index = pd.MultiIndex.from_product([MonthID, ['E', 'A', 'O']], names=['Month', 'Method'])
+    # print(index)
+    # print(new_index)
+    sol_df = pd.DataFrame(np.nan, index=[*range(1, N + 1)], columns=index)
+    sol_df = pd.DataFrame(np.nan, index=row_index, columns=index)
+    # sol_df.index.name = 'Product'
+
+    for i in ProductID:
         for t in MonthID:
             for k in Shipping_method:
-                p2.addConstr(quicksum(x[i, k, t] for i in ProductID) <= Total_demand * z[k, t], 'Z constraint')
-
-        # Y的binary constraint
-        for i in ProductID:
-            for t in MonthID:
-                p2.addConstr(Total_demand * d[i, t] >= s[i, t])
-                p2.addConstr(Total_demand * (1 - d[i, t]) >= v[i, t])
-                p2.addConstr(D[i, t] >= Demand.loc[i, t] + Backorder_percentage[i] * s[i, t - 1])
-
-        # ceiling constraints
-        for t in MonthID:
-            p2.addConstr(g[t] >= quicksum(Cubic_meter[i] * x[i, 3, t] for i in ProductID) / 0.5)
-
-        # conflict constraint
-        for i, j in Conflict_tl:
-            for t in MonthID:
-                p2.addConstr((a[i, t] + a[j, t] <= 1))
-
-        # o[r, t] constraints
-        for r in VendorID:
-            for t in MonthID:
-                for i in [ProductID_grouped.groups[r]]:
-                    p2.addConstr((quicksum(x.select(i, '*', t))) <= Total_demand * o[r, t])
-
-        # Lower Bound constraint
-        p2.addConstrs((quicksum(x.select(i, '*', t)) >= Lower_bound[i] * a[i, t]) for i in ProductID for t in MonthID)
-        p2.addConstrs((quicksum(x.select(i, '*', t)) <= Total_demand * a[i, t]) for i in ProductID for t in MonthID)
-
-        p2.optimize()
-
-        # print(f's{num}\'s objVal: {locale.currency(p2.objVal, grouping=True)}')
-        print(p2.objVal)
-
-
-        arr = []
-        arr1 = []
-        for i in range(1, M + 1):
-            for j in range(3):
-                arr1.append(str(i))
-        arr = [arr1, ['E', 'A', 'O'] * M]
-        tuples = list(zip(*arr))
-        # print(tuples)
-        index = pd.MultiIndex.from_tuples(tuples, names=['Month', 'Method'])
-
-        sol_df = pd.DataFrame(np.nan, index=[*range(1, N + 1)], columns=index)
-        sol_df.index.name = 'Product'
-
-        for i in ProductID:
-            for t in MonthID:
-                for k in Shipping_method:
-                    if k == 1:
-                        sol_df.loc[i, (str(t), 'E')] = x[i, k, t].x
-                    elif k == 2:
-                        sol_df.loc[i, (str(t), 'A')] = x[i, k, t].x
-                    elif k == 3:
-                        sol_df.loc[i, (str(t), 'O')] = x[i, k, t].x
-
-        # print(sol_df)
-        # sol_df.to_excel(f'Result_{num}.xlsx', sheet_name='Ordering Plan')
+                if k == 1:
+                    sol_df.loc[i, (str(t), 'E')] = x[i, k, t].x
+                elif k == 2:
+                    sol_df.loc[i, (str(t), 'A')] = x[i, k, t].x
+                elif k == 3:
+                    sol_df.loc[i, (str(t), 'O')] = x[i, k, t].x
+    print(p2.objVal)
+    # print(sol_df)
+    # sol_df.to_excel(f'Result_{num}.xlsx', sheet_name='Ordering Plan')
